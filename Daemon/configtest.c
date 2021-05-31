ba817d4791d56d11f0587da29afa394ecd9f99a3
Erikas Masaitis <57370157+ErikasMasaitis@users.noreply.github.com>
2021-05-31 13:57:07 +0300

    Add files via upload

--- Daemon/configtest.c
+++ Daemon/configtest.c
@@ -7,12 +7,39 @@
 #include <dirent.h>
 #include <libconfig.h>
 
+struct config {
+    struct audio *audio_type;
+    struct photo *photo_type;
+    struct document *document_type;
+    struct video *video_type; 
+    const char *dir_to_watch;
+    char *types_to_watch;
+};
+
+struct audio {
+    const char *types[10];
+    int monitor;
+};
+struct photo {
+    const char *types[10];
+    int monitor;
+};
+struct document{
+    const char *types[10];
+    int monitor;
+};
+struct video {
+    const char *types[10];
+    int monitor;
+};
+
 int main(int argc, char* argv[])
 {
     config_t cfg, *cf;
     config_setting_t *setting, *types, *types_selected;
     FILE *fp;
 
+    struct config config;
 
     const char *strs[10];
     char typeselected[40];
@@ -47,25 +74,45 @@ int main(int argc, char* argv[])
     if(config_lookup_string(&cfg, "dir_to_watch", &dir_str))
     printf("Direction to watch: %s\n\n", dir_str);
 
+        if(config_lookup_string(&cfg, "audio", &dir_str))
+    printf("Direction to watch: %s\n\n", dir_str);
+
     // gauname types to wach reikšmę
     types = config_lookup(cf, "types_to_watch");
     // kiekį elementų, kadangi list'as
     types_count = config_setting_length(types);
-
     // kiekvieno tipo išvedame 
     for(int j = 0; j < types_count; j++) {
-        
+
         strcpy(typeselected, config_setting_get_string_elem(types, j));
-        printf("%s \n", config_setting_get_string_elem(types, j));
+        printf("%s \n", typeselected);
 
         types_selected = config_lookup(cf, typeselected);
-        types_selected_count = config_setting_length(types_selected);
 
+        types_selected_count = config_setting_length(types_selected);
+        printf("tipu kiekis: %d\n", types_selected_count);
+        
         for (int i = 0; i<types_selected_count; i++) {
-            printf("\t#%d. %s\n", i + 1,
-                config_setting_get_string_elem(types_selected, i));
+            if (strcmp(config_setting_get_string_elem(types, j), "audio") == 0) {
+                             printf("tuu\t#%d. %s\n", i + 1,
+             config_setting_get_string_elem(types_selected, i));
+                config.audio_type->types[i] = config_setting_get_string_elem(types_selected, i);
+
+            } else if (strcmp(config_setting_get_string_elem(types, j), "photo") == 0)  {
+                config.photo_type->types[i] = config_setting_get_string_elem(types_selected, i);
+                
+            } else if (strcmp(config_setting_get_string_elem(types, j), "document") == 0)  {
+                 printf("tuu\t#%d. %s\n", i + 1,
+             config_setting_get_string_elem(types_selected, i));
+                config.document_type->types[i] = config_setting_get_string_elem(types_selected, i);
+                
+            } else if (strcmp(config_setting_get_string_elem(types, j), "video") == 0)  {  
+                config.video_type->types[i] = config_setting_get_string_elem(types_selected, i);
+            }
         }
     }
+
+    printf("%s\n", config.document_type->types[2]);
 
     fclose(fp);
 }
