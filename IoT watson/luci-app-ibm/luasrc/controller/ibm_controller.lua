bd9d0ff4e53241080d757904abf3bd26a9868b45
Erikas Masaitis <erikas.masaitis@ktu.edu>
2021-06-28 15:15:44 +0300

    Pastabos su pointeriais
    Pointerių pataisymai, config ir header failų pataisymai

--- IoT watson/luci-app-ibm/luasrc/controller/ibm_controller.lua
+++ IoT watson/luci-app-ibm/luasrc/controller/ibm_controller.lua
@@ -1,5 +1,5 @@
 module("luci.controller.ibm_controller", package.seeall)
 
 function index()
-	entry({"admin", "services", "IBM"}, cbi("ibm_model"), _("IBM connector"),110)
+	entry({"admin", "services", "ibm"}, cbi("ibm_model"), _("IBM connector"),110)
 end
--- IoT watson/luci-app-ibm/luasrc/model/cbi/ibm_model.lua
+++ IoT watson/luci-app-ibm/luasrc/model/cbi/ibm_model.lua
@@ -6,15 +6,19 @@ flag = section:option(Flag, "enable", "Enable", "Enable program")
 
 orgId = section:option(Value, "orgId", "Organization id")
 orgId.maxlength = 100
+orgId.required
 
 typeId = section:option(Value, "typeId", "Type id")
 typeId.maxlength = 100
+typeId.required
 
 deviceId = section:option(Value, "deviceId", "Device id")
 deviceId.maxlength = 100
+deviceId.required
 
 auth = section:option(Value, "auth", "Authentification token")
 auth.datatype = "string"
 auth.maxlength = 100
+auth.required
 
 return map
