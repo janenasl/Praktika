7fd4bd34a335e66bf42f0916a11953d1e89545b2
Erikas Masaitis <erikas.masaitis@ktu.edu>
2021-06-28 08:20:08 +0300

    valgrind leaks = 0

--- logger.h
+++
@@ -1,8 +0,0 @@
-#define LOG_INFO "INFO: "
-#define LOG_WARN "WARNING: "
-#define LOG_ERROR "ERROR: "
-
-int init_logger();
-void cleanup();
-void write_to_log(char *level, char *message);
-void write_to_log_extra(char *level, char *message, char *extra_variable);
